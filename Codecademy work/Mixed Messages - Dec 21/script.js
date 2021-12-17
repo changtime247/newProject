018ad571674b744c0a4f55a006e3d30a042d6b35
AC <86252224+moomoonnow@users.noreply.github.com>
2021-12-17 16:18:53 -0500

    Update to JS since idea change

--- Codecademy work/Mixed Messages - Dec 21/script.js
+++ Codecademy work/Mixed Messages - Dec 21/script.js
@@ -1,26 +1,111 @@
-const arr_ani = ['Rat', 'Ox', 'Tiger', 'Rabbit', 'Dragon', 'Snake', 'Horse', 'Goat', 'Monkey', 'Rooster', 'Dog', 'Pig'];
-const arr_eat = [
-    'Aussie Barbied Steak + BlooOmion',
-    'Vegan: mash, dip, scoop',
-    'Non-GMO fresh giant garden variety freeze-dried and bagged',
-    'Hella-jala-spam \'n Pineapple Pizza',
-    'Giant bowl o\' curry',
-    'Unlimited buffalo wings + craft beer',
-    'Whole tuna gutted',
-    'Should\'ve not bought SQUID',
-    'Rabbit Stew',
-    'Whole Pig Spit Roasted',
-    'NY Pizza',
-    'Jamacain Oxtail Stew'
-    ];
-const arr_loc = ['NYC', 'LA', 'Las Vegas', 'Sunny Philly', 'Somewhere in Africa', 'Somewhere in Russia', 'Macau', 'Rat\'s mansion', 'Trump Towers', 'Pig\'s sty', 'HK', 'North Pole'];
+const arr_ingred = [
+    'All purpose flour', 
+    'Butter',
+    'Chicken',
+    'Sugar',
+    'Salt',
+    'Eggs',
+    'Rice',
+    'Pork',
+    'Beef',
+    'Cheese',
+    'Garlic',
+    'Orange',
+    'Turkey',
+    'Onion',
+    'Corn',
+    'Milk',
+    'Mayonnaise',
+    'Chiles',
+    'Almonds',
+    'Bacon',
+    'Mushrooms',
+    'Coconut',
+    'Beets',
+    'Strawberries',
+    'Fennel',
+    'Lamb',
+    'Apple',
+    'Shrimp',
+    'Frozen corn',
+    'Frozen spinach',
+    'Frozen peas',
+    'Ground beef',
+    'Chicken breasts',
+];
 
-var ani = arr_ani[Math.floor(Math.random() * 12)];
-var eat = arr_eat[Math.floor(Math.random() * 12)];
-var loc = arr_loc[Math.floor(Math.random() * 12)];
+const arr_extra = [
+    'allspice',
+    'cloves', 
+    'ginger',
+    'nutmeg',
+    'coriander',
+    'dill',
+    'marjoram', 
+    'dry mustard',
+    'italian seasoning',
+    'rosemary', 
+    'sage', 
+    'tarragon', 
+    'thyme',
+    'Lemons',
+    'Fresh ginger',
+    'Shallots',
+    'Capers',
+    'Canned chiles',
+    'Prepared horseradish',
+    'Anchovy paste',
+    'Almond extract',
+    'Vanilla extract',
+    'Kosher salt',
+    'Cooking spray',
+    'Cornstarch',
+    'Confectioner\'s sugar',
+    'Honey',
+    'Brown sugar'
+];
 
-const select3 = () => {
-    return `On ` + Date() + `${ani} ate ${eat} at ${loc}`
+const arr_cuisine = [
+    'Mexican Cuisine',
+    'Italian Cuisine',
+    'Spanish Cuisine',
+    'American Cuisine',
+    'British Cuisine',
+    'Thai Cuisine',
+    'Japanese Cuisine',
+    'Chinese Cuisine',
+    'Indian Cuisine',
+    'Canadian Cuisine',
+    'Russian Cuisine',
+    'Jewish Cuisine',
+    'Polish Cuisine',
+    'German Cuisine',
+    'French Cuisine',
+    'Hawaiian Cuisine',
+    'Brazilian Cuisine',
+    'Cuban Cuisine',
+    'Tibetan Cuisine',
+    'Egyptian Cuisine',
+    'Greek Cuisine',
+    'Belgian FCuisine',
+    'Irish Cuisine',
+    'Cajun Cuisine',
+    'Portuguese Cuisine',
+    'Turkish Cuisine',
+    'Haitian Cuisine',
+    'Tahitian Cuisine',
+    'Kenyan Cuisine',
+    'Korean Cuisine',
+    'Nigerian Cuisine',
+    'Libyan Cuisine'
+];
+
+var star = arr_ingred[Math.floor(Math.random() * arr_ingred.length)];
+var extra = arr_extra[Math.floor(Math.random() * arr_extra.length)];
+var cuisine = arr_cuisine[Math.floor(Math.random() * arr_cuisine.length)];
+
+const mealToday = () => {
+    return `Chef, your challenge today is to take ${star} and ${extra} and create ${cuisine}. Good luck!`
 };
 
-console.log(select3());
+console.log(mealToday());
